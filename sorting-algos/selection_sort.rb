fd10dd1d9c39d5ce056d275d7708112542e76945
mwong068 <mwong068@gmail.com>
2020-07-07 14:11:28 -0700

    added selection sort problem

--- sorting-algos/selection_sort.rb
+++ sorting-algos/selection_sort.rb
@@ -24,3 +24,34 @@ def selection_sort(array)
   
   
   print selection_sort([5, 10, 12, 1, 2, 7])
+
+
+# selection sort problem?
+  #iterate through array two times to compare the first number to the second
+#go through array until second number is smaller than the first number
+#each time second number is found, swap it with the first one
+#need to set minimum number as placeholder
+
+# def selection_sort (array, n)
+#   i = 0
+#   while i < n-2
+#     i_min = i
+#       j = i + 1
+#       while j < n
+#         if array[j] < array[i_min]
+#           i_min = j
+#         end
+#         j+=1
+#       end
+#     i+=1
+#   end
+
+#   temp = array[i]
+#   array[i] = array[i_min]
+#   array[i_min] = temp
+
+# return array
+# end
+
+# array = [3, 1, 7, 10, 2]
+# selection_sort(array, array.size)
