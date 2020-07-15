26322deaf16f1c5132f2870de6821db6009ea33b
mwong068 <mwong068@gmail.com>
2020-07-15 15:10:34 -0700

    added recursive sum function

---
+++ algorithms/techniques/recursion.rb
@@ -0,0 +1,10 @@
+def get_sum(array)
+    size = array.length
+    if size == 1
+        return array[0]
+    else
+        return (array[0] + get_sum(array[1...size]))
+    end
+end
+
+puts get_sum([1, 2, 3, 4, 5, 6])
