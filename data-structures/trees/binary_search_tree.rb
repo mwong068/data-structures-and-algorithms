b92b6c72dd3541c287e9171f1b09f0b3737ecdd7
mwong068 <mwong068@gmail.com>
2020-07-16 15:41:12 -0700

    added binary search tree verification

--- data-structures/trees/binary_search_tree.rb
+++ data-structures/trees/binary_search_tree.rb
@@ -1,6 +1,5 @@
-class BinaryNodeTree
-    attr_accessor :value
-    attr_reader :left, :right
+class BinaryTreeNode
+    attr_accessor :value, :left, :right
   
     def initialize(value)
       @value = value
@@ -16,3 +15,49 @@ class BinaryNodeTree
       @right = BinaryTreeNode.new(value)
     end
   end
+
+  # check if BST is a BST
+
+  def push_node(root, value)
+    if value > root.value
+      if root.right
+        push_node(root.right, value)
+      else
+        root.insert_right(value)
+      end
+    end
+
+    if value < root.value
+      if root.left
+        push_node(root.left, value)
+      else
+        root.insert_left(value)
+      end
+    end
+  end
+
+
+  def traverse(node)
+    if node == nil
+      return
+    end
+
+    if node.left
+      traverse(node.left)
+    end
+    puts node.value
+    
+    if node.right
+      traverse(node.right)
+    end
+  end
+
+  array = [3, 5, 7, 2, 1, 6, 4, 9, 8]
+
+  root = BinaryTreeNode.new(array.shift)
+
+  array.each do |node|
+    push_node(root, node)
+  end
+
+  traverse(root)
