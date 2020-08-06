827ed16f718392f3917af968ddf3158b5e330971
mwong068 <mwong068@gmail.com>
2020-08-06 11:56:18 -0700

    added shortest cell path question

---
+++ problems/medium/shortest_cell_path.py
@@ -0,0 +1,16 @@
+# input:
+# grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1]]
+# sr = 0, sc = 0, tr = 2, tc = 0
+# output: 8
+# (The lines below represent this grid:)
+# 1111
+# 0001
+# 1111
+
+# grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 0, 1, 1]]
+# sr = 0, sc = 0, tr = 2, tc = 0
+# output: -1
+# (The lines below represent this grid:)
+# 1111
+# 0001
+# 1011
