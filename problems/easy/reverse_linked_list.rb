0f2239ebedf1e085b143c92dae32fb6946266e5a
mwong068 <mwong068@gmail.com>
2020-08-14 12:42:52 -0700

    added example for linked list reversal

--- problems/easy/reverse_linked_list.rb
+++ problems/easy/reverse_linked_list.rb
@@ -1,4 +1,4 @@
-def ListNode
+class ListNode
 
     attr_accessor :value, :next
 
@@ -23,3 +23,14 @@ def reverse_linked_list(head)
 
     return previous_node
 end
+
+a = ListNode.new(1)
+b = ListNode.new(2)
+c = ListNode.new(3)
+d = ListNode.new(4)
+e = ListNode.new(5)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+puts reverse_linked_list(a)
