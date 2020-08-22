42cf6a4fee7ad6570bb9b00f401f787b764977a6
mwong068 <mwong068@gmail.com>
2020-08-22 10:53:33 -0700

    added ruby guides implementation for trie

---
+++ data-structures/tries/ruby_guides.rb
@@ -0,0 +1,31 @@
+class Node
+    attr_reader   :value, :next
+    attr_accessor :word
+    def initialize(value)
+      @value = value
+      @word  = false
+      @next  = []
+    end
+  end
+  
+  class Trie
+    def initialize
+      @root = Node.new("*")
+    end
+  end
+
+  def add_word(word)
+    letters = word.chars
+    base = @root
+    letters.each { |letter| base = add_character(letter, base.next) }
+    base.word = true
+  end
+  def find_word(word)
+    letters = word.chars
+    base = @root
+    word_found =
+    letters.all? { |letter| base = find_character(letter, base.next) }
+    yield word_found, base if block_given?
+    base
+  end
+  
