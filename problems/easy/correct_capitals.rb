b4b2d5df393761c1bc6e2b90f2f852337d1dfe1c
mwong068 <mwong068@gmail.com>
2020-09-18 18:34:32 -0700

    added capital letter problem

---
+++ problems/easy/correct_capitals.rb
@@ -0,0 +1,44 @@
+def detect_capital_use(word)
+    
+    whole_capital = false
+    no_caps = false
+    one_cap = false
+        
+    if word.length == 1
+        return true
+    end
+    
+    if word[0] == word[0].capitalize && word[1] == word[1].capitalize
+        whole_capital = true
+    elsif word[0] == word[0].capitalize && word[1] != word[1].capitalize
+        one_cap = true
+    elsif word[0] != word[0].capitalize && word[1] != word[1].capitalize
+        no_caps = true
+    else
+        return false
+    end
+    
+# case 1: whole word capitalized
+    if whole_capital == true
+        i = 2
+        while i < word.length
+            if word[i] != word[i].capitalize
+                return false
+            end
+            i+=1
+        end
+    end
+    
+# case 2: no capitals & case 3: first letter capital only
+    if no_caps == true || one_cap == true
+        i = 2
+        while i < word.length
+            if word[i] == word[i].capitalize
+                return false
+            end
+            i+=1
+        end
+    end
+    
+ return true
+end
