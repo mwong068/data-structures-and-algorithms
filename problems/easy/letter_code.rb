9717caa40bcd4a688d08f7013b0ccbb91916396a
mwong068 <mwong068@gmail.com>
2020-10-08 18:55:57 -0700

    added part of solution to letter code problem

--- problems/easy/letter_code.rb
+++ problems/easy/letter_code.rb
@@ -5,3 +5,17 @@ Input: 123
 Output: 3
 
 # loop through string and determine if one integer can represent one letter, etc.
+
+def letter_code(string)
+    total = 0
+
+    i = 0
+    while i < string.length
+        if (string[i]).to_i <= 26
+            total += 1
+            if (string[i] + string[i+1]).to_i <= 26
+                total += 1
+            end        
+        end
+    end
+end
