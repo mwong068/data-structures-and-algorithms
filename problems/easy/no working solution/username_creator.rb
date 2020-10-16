dc365c6069bea3ba3d42320d94d26721838e374a
mwong068 <mwong068@gmail.com>
2020-10-16 12:07:26 -0700

    starting solution for username creator

---
+++ problems/easy/no working solution/username_creator.rb
@@ -0,0 +1,12 @@
+# Username Creator
+# [cool, coolbeans, cool, coolerbeans, coolbeans, cool, cool]
+# [cool, coolbeans, cool1, coolerbeans, coolbeans1, cool2, cool3]
+# want to add consecutive digits after the word if the name has occured already
+
+def username_creator(array)
+    username_hash = {}
+    array.each do |username|
+        if username_hash[username]
+        end
+    end
+end
--- problems/easy/username_creator.rb
+++
@@ -1,7 +0,0 @@
-# Username Creator
-# [cool, coolbeans, cool, coolerbeans, coolbeans, cool, cool]
-# [cool, coolbeans, cool1, coolerbeans, coolbeans1, cool2, cool3]
-
-def username_creator(array)
-    
-end
