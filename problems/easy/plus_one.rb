58f4810f9027d18f460aa260dc18a5c2ddc946d9
mwong068 <mwong068@gmail.com>
2021-08-28 11:00:05 -0700

    working solution for plus one problem

--- problems/easy/plus_one.rb
+++ problems/easy/plus_one.rb
@@ -1,26 +1,49 @@
-Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+# Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
 
-The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+# The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
 
-You may assume the integer does not contain any leading zero, except the number 0 itself.
+# You may assume the integer does not contain any leading zero, except the number 0 itself.
 
- 
+# Example 1:
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
 
-Example 1:
+# Example 2:
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
 
-Input: digits = [1,2,3]
-Output: [1,2,4]
-Explanation: The array represents the integer 123.
-Example 2:
-
-Input: digits = [4,3,2,1]
-Output: [4,3,2,2]
-Explanation: The array represents the integer 4321.
-Example 3:
-
-Input: digits = [0]
-Output: [1]
+# Example 3:
+# Input: digits = [0]
+# Output: [1]
 
 def plus_one(digits)
+    # if digits[-1] == 9
+    #     i = digits.length-1
+    #     while digits[i] == 9
+    #         digits[i] = 0
+    #         digits[i-1] += 1
+    #     i -= 1
+    #     end  
+    # else
+    #     digits[-1] += 1
+    # end
+
+    i = digits.length - 1
+    while i >= 0
+        digits[i] += 1
+        if digits[i] > 9
+            digits[i] = 0
+        else
+            return digits
+        end
+        i -= 1
+    end
+    digits.unshift(1)
+    return digits
 end
+
+nums = [9,9,9]
+p plus_one(nums)
 
