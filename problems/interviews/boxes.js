3f6c7ae2eb2174bedfa9d88bfc2ce5060b149334
mwong068 <mwong068@gmail.com>
2021-10-18 11:04:26 -0700

    added box units problem

---
+++ problems/interviews/boxes.js
@@ -0,0 +1,16 @@
+function getMaxUnits(boxes, unitsPerBox, truckSize) {
+    // loops through array to find the largest units per box, puts index in hash with number and corresponding amount
+    // loops through sorted hash amount of times = trucksize and multiplies key by value and adds together to get output
+    
+    let container = new Object ();
+    let maxNumber = Math.max(...unitsPerBox);
+    let maxIndex = unitsPerBox.indexOf(maxNumber);
+    container[maxNumber] = maxIndex;
+    for(var i = 0; i < unitsPerBox.length; i++){
+        if(container[unitsPerBox[i]] === undefined){
+            container[unitsPerBox[i] = i]
+        }
+    }
+    console.log(container)
+
+}
